72b4d35309e3ef21caae3c41835abb9017c1cade
eduardo_d_maia <eduardo_d_maia@estudante.sesisenai.org>
2026-02-05 16:24:19 -0300

    EnviadorDeEmail

---
+++ src/SRP_VIOLACAO/EnviadorDeEmail.java
@@ -0,0 +1,8 @@
+package SRP_VIOLACAO;
+
+public class EnviadorDeEmail {
+    public void enviarEmail(String email, String idPedido) {
+        System.out.println("Email de confirmação enviado para: " + email + " | Pedido: " + idPedido);
+    }
+}
+
